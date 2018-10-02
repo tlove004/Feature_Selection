23070759b28e51a14ce7fdb9b4ee850c529b5d48
lilott8 <lilott8@gmail.com>
2018-10-02 12:31:36 -0700

    added logging and argsparser

--- interface.py
+++ interface.py
@@ -2,15 +2,18 @@
 # format for text UI taken from project prompt
 import util
 from timeit import default_timer as timer
+import argparse
+import sys
+import logging
 
 
 def GetFile():
     """
     Get the file from the command line.
-    (I don't know why I didn't use argsparse...
+    (I don't know why I didn't use argsparse...)
     :return: File to be parsed.
     """
-    print "Welcome to Tyson Loveless' Feature Selection Algorithm."
+    print("Welcome to Tyson Loveless' Feature Selection Algorithm.")
     name = raw_input('Type in the name of the file to test: ')
     return util.parse(name)
 
@@ -20,42 +23,62 @@ def GetAlgorithm():
     Allows the user to select an algorithm to run on a dataset
     :return: algorithm to be used.
     """
-    print "Type the number of the algorithm you want to run."
-    print "\n   1)  Forward Selection"
-    print "\n   2)  Backward Elimination"
-    print "\n   3)  Tyson's Genetic Algorithm\n"
+    print("Type the number of the algorithm you want to run.")
+    print("\n   1)  Forward Selection")
+    print("\n   2)  Backward Elimination")
+    print("\n   3)  Tyson's Genetic Algorithm\n")
     search_type = input('                 ')
+    logging.info(search_type)
     return search_type
 
 
-def main():
+def get_algorithm(arg):
+    if arg.lower() == 'ss':
+        return 3
+    elif arg.lower() == 'fs':
+        return 1
+    elif arg.lower() == 'bs':
+        return 2
+
+
+def main(args):
     """
     Main method of the program, this runs the program
     :return: null
     """
-    data = GetFile()
-    search_type = GetAlgorithm()
+    data = util.parse(args.input)
 
-    n = data[0][1].__len__()
+    search_type = get_algorithm(args.algorithm)
 
-    print "\nThis dataset has " + str(n) + " features (not including the class attribute), with " \
-          + str(data.__len__()) + " instances."
+    n = len(data[0][1])
 
-    print("\nPlease wait while I normalize the data... "),
+    logging.info("This dataset has {} features (not including the class attribute), with {} instances".format(n, len(data)))
+
+    logging.info("Please wait while I normalize the data... ")
     data = util.normalize(data)
-    print "Done!"
+    logging.info("Done!")
 
     accuracy = util.nearest_neighbor(data)
-    print "\nRunning nearest neighbor with all " + str(n) + " features, using \"leave-one-out\" evaluation, I get an accuracy of " + str(accuracy*100) + "%"
+    logging.info("Running nearest neighbor with all " + str(n) + " features, using \"leave-one-out\" evaluation, I get an accuracy of " + str(accuracy*100) + "%")
 
-    print "\nBeginning search.\n"
+    logging.info("Beginning search.")
 
     start = timer()
     feature_set, accuracy = util.search(search_type, data)
     end = timer()
 
-    print "Finished search!! The best feature subset is {" + ', '.join(str(s+1) for s in feature_set) + "}, which has an accuracy of " + str(accuracy*100) + "%"
-    print "\nIt took " + str(end-start) + " seconds to find this feature set."
+    logging.info("Finished search!! The best feature subset is {" + ', '.join(str(s+1) for s in feature_set) + "}, which has an accuracy of " + str(accuracy*100) + "%")
+    logging.info("It took " + str(end-start) + " seconds to find this feature set.")
 
 
-main()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--algorithm',
+                        help="fs | bs | ss {fs=forward selection|bs=backward selection|ss=Tyson's special sauce}",
+                        default='bs', choices={'bs', 'fs', 'ss'}, required=True)
+    parser.add_argument('-i', '--input', help="Input data to be analyzed", required=True)
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    main(parser.parse_args(sys.argv[1:]))
+
